4715f7a78b8d728d6eaa06d85b9f329b0f52fd7f
Akhtar Rahat <rahatakhter4@gmail.com>
2024-11-30 23:50:06 +0600

    added ProductModel

---
+++ backend/models/ProductModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = Schema.Types;
+const shortid = require("shortid");
+
+const productSchema = new Schema(
+    {
+        autoGenId: {
+            type: String,
+            default: shortid.generate,
+        },
+        barcodeNumber: {
+            type: String,
+            index: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        unitQuantity: {
+            type: Number,
+        },
+        type: {
+            type: String,
+            enum: ["food", "grocery"],
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("products", productSchema);
