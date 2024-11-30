b832a308d8e30ca1a9e2f27202f25b4f7ad80b64
Akhtar Rahat <rahatakhter4@gmail.com>
2024-12-01 01:14:20 +0600

    added route and controller for order

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,25 @@
+const {
+    successResponse,
+    errorResponse,
+    errorHandler,
+} = require("./../utils/apiResponse");
+
+exports.placeOrder = async (req, res) => {
+    try {
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
+
+exports.getOrderListForUser = async (req, res) => {
+    try {
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
+exports.cancelOrderFromUser = async (req, res) => {
+    try {
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
---
+++ backend/routes/order/endPoint/order.user.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../../../controllers/orderController");
+
+/**
+ *
+ * @url http://localhost:5001/order
+ *
+ */
+
+router.post("/place-order", orderController.placeOrder);
+router.get("/get-orders", orderController.getOrderListForUser);
+router.put("/cancel-order", orderController.cancelOrderFromUser);
+
+module.exports = router;
