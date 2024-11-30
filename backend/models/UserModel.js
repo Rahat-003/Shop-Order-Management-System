b0aebd9466a340d243a0e86dbe16a528c89472df
Akhtar Rahat <rahatakhter4@gmail.com>
2024-11-30 23:16:53 +0600

    userModel created for different types of users

---
+++ backend/models/UserModel.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = Number(process.env.SALT_WORK_FACTOR);
+
+const UserSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        phone: {
+            type: String,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        type: {
+            type: String,
+            enum: ["admin", "manager", "customer"],
+            default: "admin",
+        },
+    },
+    { timestamps: true }
+);
+
+UserSchema.index({ email: 1 });
+
+UserSchema.pre("save", function (next) {
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+module.exports = mongoose.model("users", UserSchema);
