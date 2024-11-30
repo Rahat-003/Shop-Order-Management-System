845dd2b15d9286b890a5da9cc627fe5119b52e71
Akhtar Rahat <rahatakhter4@gmail.com>
2024-12-01 03:50:08 +0600

    added response inside controllers for test

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -6,18 +6,21 @@ const {
 
 exports.getOrdersList = async (req, res) => {
     try {
+        res.status(200).json({ message: "Successfully fetched orderList" });
     } catch (err) {
         errorHandler(res, err);
     }
 };
 exports.updateOrder = async (req, res) => {
     try {
+        res.status(200).json({ message: "Successfully updated orderList" });
     } catch (err) {
         errorHandler(res, err);
     }
 };
 exports.deleteOrder = async (req, res) => {
     try {
+        res.status(200).json({ message: "Successfully deleted orderList" });
     } catch (err) {
         errorHandler(res, err);
     }
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,6 +6,7 @@ const {
 
 exports.userSubmitForm = async (req, res) => {
     try {
+        res.status(200).json({ message: "Successfully submitted" });
     } catch (err) {
         errorHandler(res, err);
     }
@@ -13,12 +14,14 @@ exports.userSubmitForm = async (req, res) => {
 
 exports.getOrderListForUser = async (req, res) => {
     try {
+        return errorResponse(res, "successfully get");
     } catch (err) {
         errorHandler(res, err);
     }
 };
 exports.cancelOrderFromUser = async (req, res) => {
     try {
+        return errorResponse(res, "successfully cancelled");
     } catch (err) {
         errorHandler(res, err);
     }
